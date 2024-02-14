6ef476d69785702e22a2868619623303015ff1aa
Eldessouky <eldessouky.nour@gmail.com>
2024-02-14 13:30:45 -0500

    Loops

--- Week6_Iterations/loops.py
+++ Week6_Iterations/loops.py
@@ -58,9 +58,16 @@ for num in nums:
 
 for letter in 'abcde':
     print(letter)
-'''
+
 rows = int(input('Enter the number of rows: '))
 for i in range(1, rows+1):
     for j in range(1, i+1):
         print(j, end=' ')
     print()
+'''
+rows = int(input('Enter the number of rows: '))
+for i in range(0, rows):
+    for j in range(rows-i, 0, -1):
+        print(j, end=' ')
+    print()
+
